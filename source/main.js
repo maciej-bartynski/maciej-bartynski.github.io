b788348d9bf4b5438b61aff186e52b5b05678f48
JestemBartek <maciek.bartynski@gmail.com>
2018-06-21 12:27:53 +0200

    poprawione linki, startowa pozycja i opis projektu

--- source/main.js
+++ source/main.js
@@ -1,7 +1,6 @@
 //navi
 import './main-navi/navi.scss';
 import './main-navi/navi.js';
-
 //cv
 import './styles.scss';
 import './left-column/left-column_main-style.scss';
@@ -16,16 +15,30 @@ import './project-containers/converters-container/converters-container.scss';
 import './project-containers/converters-container/converters-containers.js';
 import './project-containers/keyframes-container/keyframes-container.scss';
 import './project-containers/keyframes-container/keyframes-container.js';
-
 import './project-containers/magicTxt-container/magicTxt-container.scss';
 import './project-containers/magicTxt-container/magicTxt-container.js';
-
 import './project-containers/photoFade-container/photoFade-container.scss';
 import './project-containers/photoFade-container/photoFade-container.js';
-
 import './project-containers/cvPlusPlus-container/cvPlusPlus-container.scss';
 import './project-containers/cvPlusPlus-container/cvPlusPlus-container.js';
-
+//reccomend
 import './recomendations-container/recomendations.scss';
-
+//footer
 import './footer/footer.scss';
+'use strict';
+document.addEventListener('DOMContentLoaded', setPositionOnPage);
+
+function setPositionOnPage() {
+    setTimeout(function () {
+        var isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style;
+        if (isSmoothScrollSupported === true) {
+            window.scrollTo({
+                'behavior': 'smooth',
+                'left': 0,
+                'top': 0
+            });
+        } else {
+            window.scrollTo(0, 0);
+        }
+    }, 600)
+}
