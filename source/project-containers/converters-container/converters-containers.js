64487af8be28cb94a5d1484e102c6b566aa1538a
JestemBartek <maciek.bartynski@gmail.com>
2018-06-21 10:09:10 +0200

    gpages learning

--- source/project-containers/converters-container/converters-containers.js
+++ source/project-containers/converters-container/converters-containers.js
@@ -4,7 +4,7 @@ function enableActiveLinksToConverters(){
     let converterPositioners=document.querySelectorAll('.project-container.converters .project-container_positioner');
     for (let i=0; i<converterPositioners.length;i++){
         let item = converterPositioners[i];
-        let links = ['./iframes/converters/1st/index.html', './iframes/converters/2nd/index.html', './iframes/converters/3rd/converter.html', './iframes/converters/4th/index.html']
+        let links = ['./iframes/converters/first/index.html', './iframes/converters/second/index.html', './iframes/converters/third/index.html', './iframes/converters/fourth/index.html']
         let link =links[i];
         item.addEventListener('click', function(){
             window.open(link);
