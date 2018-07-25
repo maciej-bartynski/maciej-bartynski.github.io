47553160d4cc1855ffc4609860885bbdfbb18748
JestemBartek <maciek.bartynski@gmail.com>
2018-07-25 12:24:52 +0200

    commit

--- source/recomendations-container/recomendation.js
+++ source/recomendations-container/recomendation.js
@@ -1,18 +1,31 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    let redirToPersonalProfile=document.querySelector('#b-kotrys .recomendations_see-profile-or-see-organization .recomendations_see-profile-or-see-organization_svg-container.profile');
-    let redirToOrganizationProfile=document.querySelector('#b-kotrys .recomendations_see-profile-or-see-organization .recomendations_see-profile-or-see-organization_svg-container.organization');
-    redirToPersonalProfile.addEventListener('click', function(){
-        redirectToPersonalOrInstitutionalProfile(0);
-    })
-    redirToOrganizationProfile.addEventListener('click', function(){
+    let redirToPersonalProfileBK = document.querySelector('#b-kotrys .recomendations_see-profile-or-see-organization .recomendations_see-profile-or-see-organization_svg-container.profile');
+    let redirToPersonalProfileGD = document.querySelector('#g-dawidko .recomendations_see-profile-or-see-organization .recomendations_see-profile-or-see-organization_svg-container.profile');
+    let redirToOrganizationProfileBK = document.querySelector('#b-kotrys .recomendations_see-profile-or-see-organization .recomendations_see-profile-or-see-organization_svg-container.organization');
+    let redirToOrganizationProfileGD = document.querySelector('#g-dawidko .recomendations_see-profile-or-see-organization .recomendations_see-profile-or-see-organization_svg-container.organization');
+    redirToPersonalProfileBK.addEventListener('click', function () {
+        redirectToPersonalOrInstitutionalProfile(0, 'bkot');
+    });
+    redirToPersonalProfileGD.addEventListener('click', function () {
+        redirectToPersonalOrInstitutionalProfile(0, 'gdaw');
+    });
+    redirToOrganizationProfileBK.addEventListener('click', function () {
         redirectToPersonalOrInstitutionalProfile(1);
-    })
+    });
+    redirToOrganizationProfileGD.addEventListener('click', function () {
+        redirectToPersonalOrInstitutionalProfile(1);
+    });
 });
-function redirectToPersonalOrInstitutionalProfile(x){
-    if(x===0){
-        window.open('https://www.linkedin.com/in/bkotrys/');
-    }else{
+
+function redirectToPersonalOrInstitutionalProfile(x, who) {
+    if (x === 0) {
+        if (who === 'bkot') {
+            window.open('https://www.linkedin.com/in/bkotrys/');
+        } else if (who === 'gdaw') {
+            window.open('https://www.linkedin.com/in/grzegorz-dawidko-a0552b107/');
+        }
+    } else if (x === 1) {
         window.open('http://dev-army.pl/');
     }
 }
